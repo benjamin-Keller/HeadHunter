c0a4bc7ca2e6cf1c0842fbf09bed19c959d8cfb2
Benjamin Keller <bk.benjaminkeller@gmail.com>
2022-12-16 13:29:18 +0200

    Adds Local Auth poco

---
+++ HeadHunter/Shared/Auth/RiotUser.cs
@@ -0,0 +1,8 @@
+﻿namespace HeadHunter.Shared.Auth
+{
+    public class RiotUser
+    {
+        public string Username { get; set; } = "Nerrrry";
+        public string Password { get; set; } = "Ben102030*";
+    }
+}
