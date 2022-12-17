9049c9aab054b081e8c163ed20441a5737f501ec
Benjamin Keller <bk.benjaminkeller@gmail.com>
2022-12-17 02:10:25 +0200

    Adds Auth Endpoint

---
+++ HeadHunter/Server/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using HeadHunter.Server.Services;
+using HeadHunter.Shared.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeadHunter.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _service;
+
+        public AuthController(AuthService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Authenticate([FromForm] RiotUser riotUser)
+        {
+            var userInfo = await _service.AuthenticateAsync(riotUser);
+            return Ok(userInfo);
+        }
+    }
+}
